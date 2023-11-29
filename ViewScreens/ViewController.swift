40a50745d4b6323a25a6bc5026d6e4bb13a412d1
Nur <mirrorvol@gmail.com>
2023-11-29 11:51:39 +0400

    Initial Commit

---
+++ ViewScreens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewScreens
+//
+//  Created by Nur on 29.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
